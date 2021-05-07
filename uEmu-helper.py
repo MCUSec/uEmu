7708455f51daf86a23638501055008fe4c9d8468
weizhou-chaojixx <weizhoulightning@gmail.com>
2021-05-07 09:58:21 +0800

    script update
    Signed-off-by: weizhou-chaojixx <weizhoulightning@gmail.com>

--- uEmu-helper.py
+++ uEmu-helper.py
@@ -11,7 +11,7 @@ import configparser
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 DEFAULT_TEMPLATES_DIR = os.getcwd()
-def read_config(cfg_f, mode, cachefilename, firmwarename, debug):
+def read_config(cfg_f, firmwarename, debug):
     if not os.path.isfile(cfg_f):
         sys.exit("Cannot find the specified configuration file: %s" % cfg_f)
     parser = configparser.SafeConfigParser()
@@ -23,50 +23,12 @@ def read_config(cfg_f, mode, cachefilename, firmwarename, debug):
         'creation_time': str(datetime.datetime.now()),
         'firmware_name': firmwarename,
         'pwd': str(os.getcwd()),
-        # Configure the uEmu is in fuzzing(true) phase or KB(false) extraction phase
-        'mode': "true" if mode else "false",
-        # If using fuzzing phase, the KB file name should be configured
-        'cache_file_name': cachefilename if mode else "",
     }
 
     # MEM
     config['rom'] = parser.get("MEM_Config","rom").split( )
     config['ram'] = parser.get("MEM_Config","ram").split( )
     config['vtor'] = int(parser.get("MEM_Config","vtor"), 16)
-
-    # IRQ
-    config['irq_tb_break'] = parser.getint("IRQ_Config","irq_tb_break")
-    config['disable_irqs'] = parser.get("IRQ_Config","disable_irqs").split()
-    config['disable_systick'] = parser.get("IRQ_Config","disable_systick")
-    if config['disable_systick'] == "true":
-        config['systick_begin_point'] = parser.get("IRQ_Config","systick_begin_point")
-
-    # Invlid states detection
-    config['bb_inv1'] = parser.getint("INV_Config","bb_inv1")
-    config['bb_inv2'] = parser.getint("INV_Config","bb_inv2")
-    config['bb_terminate'] = parser.getint("INV_Config","bb_terminate")
-    config['kill_points'] = parser.get("INV_Config","kill_points").split()
-    config['alive_points'] = parser.get("INV_Config","alive_points").split()
-
-    # TC
-    config['t2_function_parameter_num'] = parser.getint("TC_Config","t2_function_parameter_num")
-    config['t2_caller_level'] = parser.getint("TC_Config","t2_caller_level")
-    config['t2_max_context'] = parser.getint("TC_Config","t2_max_context")
-    config['t3_max_symbolic_count'] = parser.getint("TC_Config","t3_max_symbolic_count")
-
-    # Fuzzing target
-    if config['mode'] == "true":
-        config['enable_fuzz'] = parser.get("Fuzzer_Config","enable_fuzz")
-        config['allow_auto_mode_switch'] = parser.get("Fuzzer_Config","allow_auto_mode_switch")
-    else:
-        config['enable_fuzz'] = "false"
-    if config['enable_fuzz'] == "true":
-        config['additional_writable_ranges'] = parser.get("Fuzzer_Config","additional_writable_ranges").split( )
-        config['input_peripherals'] = parser.get("Fuzzer_Config","input_peripherals").split( )
-        config['time_out'] = parser.getint("Fuzzer_Config","time_out")
-        config['crash_points'] = parser.get("Fuzzer_Config","crash_points").split()
-        config['allow_new_phs'] = parser.get("Fuzzer_Config","allow_new_phs")
-        config['fork_count'] = parser.get("Fuzzer_Config","fork_count")
 
     return config
 
@@ -118,10 +80,6 @@ def main(argv):
                             help="Configure the configuration file used for Knowledge Base extration. e.g., uEmu.cfg")
     parser.add_argument("--debug", action="store_true",
                             help="In debug mode, uEmu will output huge log, please ensure you have enough space (e.g., more than 100M)")	
-    parser.add_argument("-kb", "--KBfilename", type=str, default="",
-                            help="Configure the Knownledge Base filename used for uEmu")
-    parser.add_argument("-s", "--seedfilename", type=str, default="",
-                            help="Configure the fuzzing seed filename used for AFL fuzzer")
 
     args = parser.parse_args()
 
@@ -130,49 +88,20 @@ def main(argv):
     else:
         sys.exit("Please set configuration .cfg file!")
 
-    if args.KBfilename == "":
-        if args.debug:
-            print("uEmu will run %s in knowledge extraction phase with debug level log" % (args.firmware))
-        else:
-            print("uEmu will run %s in knowledge extraction phase with warning level log" % (args.firmware))
-        mode = False
-    else:
-        if args.seedfilename == "":
-            list_random = []
-            for num in range(0,4):
-                randomseed = random.randint(0,255)
-                list_random.append(randomseed)
-            with open('testcase', 'wb') as f:
-                for r in list_random:
-                    rd = struct.pack('B', r)
-                    f.write(rd)
-            seedfilename = 'testcase'
-        else:
-            seedfilename = args.seedfilename
-            print("uEmu will use manual seed file %s in dynamic analysis (fuzzing) phase" % (args.seedfilename))
-        afl = {
-            'creation_time': str(datetime.datetime.now()),
-            'firmware': args.firmware,
-            'seed': seedfilename,
-        }
-        render_template(afl, "launch-AFL-template.sh", "launch-AFL.sh", executable=True)
-        if args.debug:
-            print("uEmu will run %s in dynamic analysis (fuzzing) phase with debug level log" % (args.firmware))
-        else:
-            print("uEmu will run %s in dynamic analysis (fuzzing) phase with warning level log" % (args.firmware))
-        mode = True
-
-    config = read_config(args.config, mode, args.KBfilename, args.firmware, args.debug)
+    config = read_config(args.config, args.firmware, args.debug)
     render_template(config, "uEmu-config-template.lua", "uEmu-config.lua")
     launch = {
         'creation_time': str(datetime.datetime.now()),
         'qemu_arch':"arm",
         'memory':"2M",
         'firmware': args.firmware,
-        'root_dir': os.environ['uEmuDIR'],
+        'root_dir': os.environ['S2EDIR'],
     }
     render_template(launch, "launch-uEmu-template.sh", "launch-uEmu.sh", executable=True)
-
+    if args.debug:
+        print("%s has been configured with debug level log" % (args.firmware))
+    else:
+        print("%s has been configured with warning level log" % (args.firmware))
 
 if __name__ == "__main__":
     # sys.argv[1:]为要处理的参数列表，sys.argv[0]为脚本名，所以用sys.argv[1:]过滤掉脚本名
